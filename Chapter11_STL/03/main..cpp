128c1e83623cf7829fa4c74d2d8bd0f521f13db8
decemberkms <decemberkms@gmail.com>
2021-12-17 16:04:01 +0100

    Add till iterator

--- Chapter11_STL/03/main..cpp
+++ Chapter11_STL/03/main..cpp
@@ -1,8 +1,73 @@
 #include <iostream>
-#include <stirng>
+#include <string>
 
-class Array{
-    int size {N};
-    int values[N];
+// Note: Since C++ 11 the STL has std::array which is a template-based array class
+// Use std::array instead of raw arrays whenever possible
+
+// Note that we can have non-type template parameters
+// That's what N is in this class
+
+
+template <typename T, int N>
+class Array {
+private:
+    int size {N}; // how do we get the N??
+    T values[N]; // the N needs to be known at compile-time!;
     
+    friend std::ostream &operator <<(std::ostream &os, const Array<T, N> &arr){
+        os << "[ ";
+        for (const auto &val:arr.values)
+            os << val << " ";
+        os << " ]" << std::endl;
+        return os;
+    }
+public:
+    Array() = default;
+    Array(T init_val){
+        for (auto &item:values)
+            item = init_val;
+    }
+    void fill(T val){
+        for (auto &item:values)
+            item = val;
+    }
+    int get_size() const{
+        return size;
+    }
+    //overloaded subscript operator for easy use
+    T &operator[](int index){
+        return values[index];
+    }
+};
+
+int main(){
+    Array<int, 5> nums;
+    std::cout << "The size of nums is " << nums.get_size() << std::endl;
+    
+    nums.fill(0);
+    std::cout << "The size of nums is " << nums.get_size() << std::endl;
+    std::cout << nums << std::endl;
+
+    nums.fill(10);
+    std::cout << nums << std::endl;
+
+    nums[0] = 1000;
+    nums[3] = 2000;
+    std::cout << nums << std::endl;
+
+    Array<int, 100> nums2 {1};
+    std::cout << "The size of nums2 is " << nums2.get_size() << std::endl;
+    std::cout << nums2 << std::endl;
+
+
+    Array<std::string, 10> strings{std::string{"Minsung"}};
+    std::cout << "The size of nums2 is " << strings.get_size() << std::endl;
+    std::cout << strings << std::endl;
+    
+    strings[0] = std::string{"Kim"};
+    std::cout << strings << std::endl;
+
+    strings.fill(std::string{"Z"});
+    std::cout << strings << std::endl;
+    return 0;
 }
